7d66ee83fad49cb63c379f179217dfb7442781c9
chiconasa3000 <csucavelando@gmail.com>
2018-11-01 18:42:47 -0500

    fixCompilation

--- GenDRRs/genVirtualImage.cxx
+++ GenDRRs/genVirtualImage.cxx
@@ -1,28 +1,24 @@
-
 /* Generador de imagenes virtuales usando interpolador Patched */
 
-
 #include <iostream>
-
-
+#include <itkImage.h>
+#include "../itkPatchedRayCastInterpolateImageFunction.h"
 // funcion de menu principal 
 
-namespace{
+
 void menu(){
 	std::cout<<"Generador de imagenes virtuales : Parametros necesitados"<< std::endl;
-}	
-
-
+}
 
 int main(int argc, char *argv[]){
 	//function con las terceras parte
-	
+	menu();	
 	const unsigned int Dimensions = 3;
 
-	typedef itk::Image<short int, Dimensions> FixedImageType;
+	typedef itk::Image<short int,Dimensions> FixedImageType;
 	typedef itk::Image<short int,Dimensions> MovingImageType;
-	typedef itk::PatchedRayCastInterpolateImageFunction<MovingimageType, double> InterpolateType;
-	IntepolatorType::Pointer interpolator = InterpolatorType::New();
+	typedef itk::PatchedRayCastInterpolateImageFunction<MovingImageType, double> InterpolatorType;
+	InterpolatorType::Pointer interpolator = InterpolatorType::New();
 	return 0;
 }
-}
+
